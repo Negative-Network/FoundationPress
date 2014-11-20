0526bb0a8aa2466bc82e69667b521c73b01fec98
Pierre Blarre <pierre@negative-network.com>
2014-11-20 15:39:37 +0100

    adding default Themefuse scratch-parent framework-customizations

---
+++ framework-customizations/extensions/styling/extensions/switch-style-panel/config.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$cfg = array();
+
+// enable or disable panel
+$cfg['display'] = true;
+
+// the text that will be displayed at the top of the panel
+$cfg['description'] = __('Choose a style:', 'fw');
