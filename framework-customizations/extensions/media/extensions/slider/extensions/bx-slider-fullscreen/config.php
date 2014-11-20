746ccec2d839c8cdd2f14e639f6086be1326525a
Pierre Blarre <pierre@negative-network.com>
2014-11-20 10:43:21 +0100

    working on a fullscreen bx slider

--- framework-customizations/extensions/media/extensions/slider/extensions/bx-slider-fullscreen/config.php
+++ framework-customizations/extensions/media/extensions/slider/extensions/bx-slider-fullscreen/config.php
@@ -1,3 +1,7 @@
-<?php if (!defined('FW')) die('Forbidden');
-$cfg['multimedia_types'] = array('image','video');
+<?php if (!defined('FW')) {
+	die('Forbidden');
+}
+
+$cfg['multimedia_types'] = array('image', 'video');
+
 //$cfg['multimedia_types'] = array('image');
--- framework-customizations/extensions/media/extensions/slider/extensions/bx-slider-fullscreen/views/bx-slider-fullscreen.php
+++ framework-customizations/extensions/media/extensions/slider/extensions/bx-slider-fullscreen/views/bx-slider-fullscreen.php
@@ -1,75 +1,120 @@
-<?php if (!defined('FW')) die('Forbidden'); ?>
-<?php if (isset($data['slides'])): ?>
-    <script type="text/javascript">
+<?php if (!defined('FW')) {
+	die('Forbidden');
+}
+?>
+<?php if (isset($data['slides'])):
 
-        var slider;
-        var images = [
-    <?php
-    foreach ($data['slides'] as $slide)
-    {
-	echo '"' . $slide['src'] . '",';
-    }
-    ?>
-        ];
-        var index = 0;
-        var transitionSpeed = 500;
-        var imageIntervals = 5000;
-        var startIntervals;
-        var intervalSetTime;
+	print_r($data);
 
-        jQuery(document).ready(function () {
+	?>
+	<script type="text/javascript">
 
-	    slider = jQuery('#slider1').bxSlider({
-		mode: 'fade',
-		controls: false,
-		pause: imageIntervals
-	    });
+						        var slider;
+						        var images = [
+	<?php
+	foreach ($data['slides'] as $slide) {
+		echo '"' . $slide['src'] . '",';
+	}
+	?>
+						        ];
+						        var index = 0;
+						        var transitionSpeed = 500;
+						        var imageIntervals = 5000;
+						        var startIntervals;
+						        var intervalSetTime;
+								var contentOpen = false;
 
-	    jQuery(function () {
+						        jQuery(document).ready(function () {
 
-		jQuery.preload(images, {
-		    init: function (loaded, total) {
-			//    		    jQuery("#indicator").html("<img src='<?php echo get_template_directory_uri() ?>images/load.gif' />");
-		    },
-		    loaded_all: function (loaded, total) {
-			jQuery('.content-container').show();
-			jQuery('#indicator').fadeOut('slow', function () {
-			    //    			jQuery('#left-side').fadeIn('slow');
-			    //    			jQuery('#thumb-container').fadeIn('slow');
-			    jQuery.backstretch(images[index], {speed: transitionSpeed});
-			    startIntervals = function () {
-				intervalSetTime = setInterval(function () {
-				    index = (index >= images.length - 1) ? 0 : index + 1;
-				    jQuery.backstretch(images[index]);
-				    slider.goToNextSlide()
-				}, imageIntervals)
-			    };
-			    startIntervals();
-			});
-		    }
-		});
-	    });
-        });
+									jQuery('header h1.entry-title').hide();
 
-    </script>
+								    slider = jQuery('#bxslider-fullscreen').bxSlider({
+									mode: 'fade',
+									controls: true,
+									pager: true,
+									pause: imageIntervals,
+									onSlideBefore : function($slideElement, oldIndex, newIndex){
+										goToContent(newIndex)
+									}
+								    });
+
+								    jQuery(function () {
+										jQuery.preload(images, {
+										    init: function (loaded, total) {
+											//    		    jQuery("#indicator").html("<img src='<?php echo get_template_directory_uri()?>images/load.gif' />");
+										    },
+										    loaded_all: function (loaded, total) {
+
+											jQuery('div.bx-wrapper').css('margin-top',jQuery(window).height()/4);
+
+											// jQuery('.content-container').show();
+											jQuery('#indicator').fadeOut('slow', function () {
+
+												$('#thumb-container').fadeIn('slow');
+											    jQuery.backstretch(images[index], {speed: transitionSpeed});
+
+											    startIntervals = function () {
+												intervalSetTime = setInterval(function () {
+												    index = (index >= images.length - 1) ? 0 : index + 1;
+												    jQuery.backstretch(images[index]);
+												    slider.goToNextSlide()
+												}, imageIntervals)
+											    };
+											    startIntervals();
+
+											});
+										    }
+										});
+								    });
+
+						        });
 
 
-<!--    <div id="left-side">
-        <a href="javascript:showContent();" class="content-button"></a>
-    </div>-->
-    <div id="indicator"></div>
 
-    <div id="slider1" class="bx-sliders">
-	<?php foreach ($data['slides'] as $slide) : ?>
-	    <div class="content-container">
-		<div class="content-inner">
-		    <h2><?php echo $slide['title']; ?></h2>
-		    <?php echo $slide['desc']; ?>
-		</div>
-	    </div>
-	<?php endforeach; ?>
-    </div>
+						function goToContent(slideNum){
+							clearInterval(intervalSetTime);
+							index = slideNum;
+							$.backstretch(images[index]);
+							slider.goToSlide(slideNum);
+							if (contentOpen == false){
+								startIntervals();
+							}
+						};
 
-    <!--<div id="thumb-container"></div>-->
+						function showContent(){
+							$('.content-bg').fadeIn('slow');
+							clearInterval(intervalSetTime);
+							contentOpen = true;
+						};
+						function closeContent(){
+							$('.content-bg').fadeOut('slow');
+							startIntervals();
+							contentOpen = false;
+						};
 
-<?php endif; ?>
+						    </script>
+
+
+						    <div id="indicator"></div>
+
+						    <div id="bxslider-fullscreen" class="bx-sliders">
+	<?php
+	$i = 0;
+	foreach ($data['slides'] as $slide):
+	?>
+							    <div class="bx-content-container">
+								<div class="bx-content-inner" id="bx-content-<?php echo $i;?>">
+	<?php echo isset($data['settings']['post_id']) ? '<a href="' . post_permalink($data['settings']['post_id']) . '">Link</a>' : '';?>
+								    <h2><?php echo $slide['title'];?></h2>
+								    <div class="desc">
+	<?php echo $slide['desc'];?>
+	</div>
+						</div>
+							    </div>
+	<?php
+	$i++;
+	endforeach;
+	?>
+	</div>
+
+	<?php endif;?>
