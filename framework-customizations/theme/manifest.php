9a282a9b70b496224b71cf997f47c11b90019d20
Florian Rodriguez <florian@negative-network.com>
2015-01-07 16:38:40 +0100

    add default extentions

---
+++ framework-customizations/theme/manifest.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$manifest = array();
+
+$manifest['id'] = 'foundationpress';
+
+$manifest['supported_extensions'] = array(
+	'analytics' => array(),
+	'portfolio' => array(),
+	'page-builder' => array(),
+	'slider' => array(),
+	'seo' => array(),
+);
